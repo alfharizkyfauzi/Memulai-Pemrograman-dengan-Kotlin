1b7376f1f9f2c7fdd7a8e02ab4092cc5fc59bdd1
alfharizkyfauzi <54635440+alfharizkyfauzi@users.noreply.github.com>
2020-06-07 02:34:05 +0700

    second commit

---
+++ Data Class dan Collections/Latihan/latihan/src/Task.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    // TODO 1
+    val user = mapOf<String, String>(
+            "Name" to "John",
+            "Age" to "23",
+            "Address" to "St. Petersburg"
+    )
+
+    // TODO 2
+    val name  = user.getValue("Name")
+    val age = user.getValue("Age")
+    val address = user.getValue("Address")
+
+    // TODO 3
+    println("User")
+    for (key in user.keys){
+    println("$key: ${user[key]}")
+    }
+}
